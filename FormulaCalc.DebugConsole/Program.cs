93499255b94906fc003ff6802f39b20818c0d317
joelesperance <lesperance.joe@gmail.com>
2023-03-12 10:09:57 -0400

    Add project files.

---
+++ FormulaCalc.DebugConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FormulaCalc.DebugConsole
+{
+    using NCalc;
+    using ExcelDna.Integration;
+    using Microsoft.Office.Interop.Excel;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application excelApp = new Application();
+
+           // Workbook workbook = excelApp.Workbooks.Open("");
+
+            double sum = excelApp.WorksheetFunction.Sum(2, 2);
+
+            Console.WriteLine(sum);
+
+            // ExcelDna
+
+
+
+
+
+
+            // NCalc
+
+            //while (true)
+            //{
+
+            //    Console.Write("X: ");
+            //    double x = double.Parse(Console.ReadLine());
+
+            //    Console.Write("Y: ");
+            //    double y = double.Parse(Console.ReadLine());
+
+            //    add.Parameters["x"] = x;
+            //    add.Parameters["y"] = y;
+
+            //    Console.WriteLine(add.Evaluate());
+
+            //}
+        }
+    }
+}
---
+++ FormulaCalc.Web.UI/Controllers/CalcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaCalc.Web.UI.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
